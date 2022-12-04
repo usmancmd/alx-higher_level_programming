3451de27ae01aab15040d02d80cb789558d11fcc
Adepoju Usman <adepojuusman465@gmail.com>
2022-12-04 18:22:25 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        cp = my_list.copy()
+        return cp
+
+    i = len(my_list)
+    if idx > i - 1:
+        return cp
+
+    cp1 = my_list[:]
+    cp1[idx] = element
+    return cp1
