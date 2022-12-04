736cab6c9357c1f8fddaf932924f72abd4d263f4
Adepoju Usman <adepojuusman465@gmail.com>
2022-12-04 12:24:41 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return (none)
+        return None
 
     i = len(my_list)
     if idx > i - 1:
-        return (none)
+        return None
 
     return (my_list[idx])
