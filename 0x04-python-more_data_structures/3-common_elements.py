240bbcf1bc101cfea4a43ec587e97afe3765f677
usmancmd <110719750+usmancmd@users.noreply.github.com>
2022-12-09 11:09:20 +0100

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                common = j
+    common = set_1 & set_2
     return common
