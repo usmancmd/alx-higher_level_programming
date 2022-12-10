be5620e06137acc22865820582dee1f4afb647cb
Adepoju Usman <adepojuusman465@gmail.com>
2022-12-10 11:34:18 +0100

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for key, val in a_dictionary.items():
+            if val == value:
+                del a_dictionary[key]
+                break
+
+    return (a_dictionary)
