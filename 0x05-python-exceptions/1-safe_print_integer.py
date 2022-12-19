bed4f8755d470008774b591c4f20d72135787224
Adepoju Usman <adepojuusman465@gmail.com>
2022-12-19 16:06:15 +0000

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if value == int(value):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+        if value != value:
+            return False
