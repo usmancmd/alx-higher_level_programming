3ec6d32b99869912c76870f706346b26fb054b3e
Adepoju Usman <adepojuusman465@gmail.com>
2022-12-21 20:47:44 +0000

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Create class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size # size of the square
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
