35159305efee7d3f350e25efa1b763f58f412991
Adepoju Usman <adepojuusman465@gmail.com>
2022-12-21 18:45:32 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Create class Square that defines a square"""
+
+class Square:
+    """Class Square that defines a square"""
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  # size of square
