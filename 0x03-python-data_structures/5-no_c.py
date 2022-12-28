992f31c7ca9bb55c4f76d55695d25a407939cc7e
usmancmd <110719750+usmancmd@users.noreply.github.com>
2022-12-28 23:27:12 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [i for i in my_string if i != "c" and i != "C"]
-    return "".join(new_string)
+#    new_string = [i for i in my_string if i != "c" and i != "C"]
+#    return "".join(new_string)
+    new = my_string[:]
+    for i in new:
+        if i == "c" or i == "C":
+            continue
+        return new
