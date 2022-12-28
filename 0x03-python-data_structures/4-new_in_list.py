028b0b734ab92f1a8d354222fd40f17a1d81c656
usmancmd <110719750+usmancmd@users.noreply.github.com>
2022-12-28 10:58:06 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,11 @@ def new_in_list(my_list, idx, element):
     if idx > i - 1:
         return my_list
 
-    cp1 = [i for i in my_list]
-    cp1[idx] = element
-    return cp1
+    #cp1 = [i for i in my_list]
+    #cp1[idx] = element
+    #return cp1
+    new_list = my_list[:]
+    for i in new_list:
+        if new_list[idx] == i:
+            new_list = element
+        return new_list
