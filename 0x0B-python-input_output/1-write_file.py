fa6a31939856843bc6e4d1956672f073a05b42da
Adepoju Usman <adepojuusman465@gmail.com>
2023-01-10 16:44:33 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=""):
     """writes to a text file"""
     with open(filename, mode="w", encoding="utf-8") as file_obj:
-        return file_obj.write(text)
+        out = file_obj.write(text)
+    return out
