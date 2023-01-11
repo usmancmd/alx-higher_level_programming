21e9662456238f7c3a1f8778a9e8b43fff04e2c9
Adepoju Usman <adepojuusman465@gmail.com>
2023-01-11 02:32:14 +0000

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,7 +8,8 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, 'r', encoding="utf-8") as file_obj:
         for line in file_obj:
             new += line
-            if search_string == line:
+            if line.find(search_string) != -1:
                 new += new_string
+
     with open(filename, 'w', encoding='utf-8') as obj:
         obj.write(new)
