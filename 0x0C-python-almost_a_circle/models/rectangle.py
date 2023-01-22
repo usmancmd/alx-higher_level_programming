aa0b7cad0ce11eb66c62c8d025d91f049c67ac81
Adepoju Usman <adepojuusman465@gmail.com>
2023-01-22 11:19:51 +0000

    Add documentry to area function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Return width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Update width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -26,10 +28,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Return height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Update height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -38,10 +42,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Return x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Update x"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -50,10 +56,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Return y"""
         return self.__x
 
     @y.setter
     def y(self, value):
+        """Update y"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -65,6 +73,7 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        """Display rectangle"""
         if self.__width == 0 or self.__height == 0:
             print("")
             return
@@ -76,6 +85,7 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
+        """Print str"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.__x,
                                                        self.__y,
@@ -83,6 +93,7 @@ class Rectangle(Base):
                                                        self.__height)
 
     def update(self, *args):
+        """Display func"""
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -120,6 +131,7 @@ class Rectangle(Base):
                     self.__y = v
 
     def to_dictionary(self):
+        """Return dictionary"""
         return {"id": self.id,
                 "width": self.width,
                 "height": self.height, "x": self.x, "y": self.y}
