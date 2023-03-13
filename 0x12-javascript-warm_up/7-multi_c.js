c4f4579762947c187284f9b3c4f477e174eda67e
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-13 23:56:40 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 
 const args = process.argv;
 const intArg = parseInt(args[2]);
-if (intArg) {
-  for (let i = 0; i <= intArg; i++) {
+if (isNaN(intArg)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < intArg; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
