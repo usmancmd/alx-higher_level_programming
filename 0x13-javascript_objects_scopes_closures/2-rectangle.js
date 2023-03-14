82831a0b881ac221edcc29d41b0eb0b56f1cfbdd
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-14 16:19:03 +0000

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,6 +2,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
+    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
+      return;
+    }
     this.width = w;
     this.height = h;
   }
