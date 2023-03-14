70f9caab09f7423e94057d2d9432fe10b247b9e4
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-14 06:28:14 +0000

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const a = parseInt(args[2])
-const b = parseInt(args[3])
-
+const a = parseInt(args[2]);
+const b = parseInt(args[3]);
 
 function add (a, b) {
   console.log(a + b);
