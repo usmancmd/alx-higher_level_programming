6c65819a720edef5315ad59edc45e28636590191
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-18 06:03:25 +0000

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 script that lists all states from the database hbtn_0e_0_usa
 it take 3 arguments: mysql username, mysql password and database name
@@ -7,15 +7,15 @@ it take 3 arguments: mysql username, mysql password and database name
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+	connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	cur = connect.cursor()
+	cur.execute("SELECT * FROM `states`")
 
-cur = connect.cursor()
-cur.execute("SELECT * FROM `states`")
+	query_rows = cur.fetchall()
+	for state in query_rows:
+		print(state)
 
-query_rows = cur.fetchall()
-for state in query_rows:
-	print(state)
-
-cur.close()
-connect.close()
+	cur.close()
+	connect.close()
