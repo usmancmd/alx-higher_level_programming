2791b010a902388369cd2fa30079d3a30dd980d6
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-18 16:56:05 +0000

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,6 +9,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+	""" to avoid executing the file when imported """
 	connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
 	cur = connection.cursor()
