13d91d2d9416a913d33bb6ef071efe0dc1c67cca
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-19 01:13:31 +0000

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa
+it take 3 arguments: mysql username, mysql password and database name
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+	""" not be executed when imported """
+	connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+	cur = connect.cursor()
+	cur.execute("SELECT  `cities`.`name`, FROM `cities` INNER JOIN `states` ON `cities`.`state_id`=`states`.`id` WHERE `states`.`name`= %s")
+
+	query_rows = cur.fetchall()
+	for state in query_rows:
+		print(state)
+
+	cur.close()
+	connect.close()
