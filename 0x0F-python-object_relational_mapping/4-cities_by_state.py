25d0fb129056fb4dfb4dd94acbabfbb9b49f7cca
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-19 00:35:54 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ import MySQLdb
 
 if __name__ == "__main__":
 	""" not be executed when imported """
-	connect = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
 	cur = connect.cursor()
 	cur.execute("SELECT * FROM `cities`")
