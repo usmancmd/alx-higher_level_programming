dc7c96c0e7cb859ce3906ec773aa763973f66e34
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-19 23:28:36 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,11 +13,25 @@ if __name__ == "__main__":
 	connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
 	cur = connect.cursor()
-	cur.execute("SELECT  `cities`.`name`, FROM `cities` INNER JOIN `states` ON `cities`.`state_id`=`states`.`id` WHERE `states`.`name`= %s")
+#	cur.execute("SELECT  `cities`.`name`, FROM `cities` INNER JOIN `states` ON `cities`.`state_id`=`states`.`id` WHERE `states`.`name`= %s", sys.argv[4])
+	query = "SELECT cities.name \
+			FROM cities \
+			INNER JOIN states ON cities.state_id=states.id \
+			WHERE (states.name='{}')".format(sys.argv[4])
 
-	query_rows = cur.fetchall()
-	for state in query_rows:
-		print(state)
+#	cur.execute(query, (sys.argv[4],))
+	cur.execute(query)
+	state = cur.fetchall()
+	length = len(state)
+
+	for cities in state:
+		for city in cities:
+			length -= 1
+			print(city, end="")
+			if (length != 0):
+				print(", ", end="")
+			else:
+				print()
 
 	cur.close()
 	connect.close()
