1883c48df1bd5ec5ab309214fc63456d8236e821
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-20 07:49:47 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cur = connect.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
                 FROM  cities \
-                INNER JOIN ON cities.state_id = states.id \
+                INNER JOIN states ON cities.state_id = states.id \
                 ORDER BY cities.id")
 
     query_rows = cur.fetchall()
