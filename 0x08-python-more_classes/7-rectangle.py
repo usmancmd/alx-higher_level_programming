080c093fc631574674911321999476fe307d125f
Usman Adepoju <adepojuusman465@gmail.com>
2023-04-28 04:23:53 +0100

    7-rectangle.py

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """initialise Rectangle"""
+
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0:
+            return 0
+        elif self.height == 0:
+            return 0
+        else:
+            return self.width + self.width + self.height + self.height
+
+    def __str__(self):
+        if (self.__width == 0 or self.__height == 0):
+            return ("")
+        return ("\n".join(["".join([str(self.print_symbol)
+                for i in range(self.__width)]) for j in range(self.__height)]))
+
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
