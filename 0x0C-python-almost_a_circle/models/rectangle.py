b81b6ebb0e478b219b47bb7924404f39a659b41e
Usman Adepoju <adepojuusman465@gmail.com>
2023-04-30 18:44:11 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,11 +4,11 @@
 class Rectangle(Base):
 	"""Rectangle class"""
 	def __init__(self, width, height, x=0, y=0, id=None):
-		super()__init__(id)
 		self.__width = width
 		self.__height = height
 		self.__x = x
 		self.__y = y
+		super().__init__(id)
 
 	@property
 	def width(self):
