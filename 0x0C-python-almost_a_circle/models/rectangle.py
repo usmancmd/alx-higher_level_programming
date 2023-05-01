0b4f0512c8dfd9f29d07e6d958f5515590785408
Usman Adepoju <adepojuusman465@gmail.com>
2023-05-01 16:55:26 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,30 +70,30 @@ class Rectangle(Base):
 
     def area(self):
         """Return area of the rectangle"""
-        return self.__width * self.__height
+        return self.width * self.height
 
     def display(self):
         """Display rectangle"""
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             print("")
             return
 
-        for y in range(self.__y):
+        for y in range(self.y):
             print("")
-        for h in range(self.__height):
-            for x in range(self.__x):
+        for h in range(self.height):
+            for x in range(self.x):
                 print(" ", end="")
-            for w in range(self.__width):
+            for w in range(self.width):
                 print("#", end="")
             print("")
 
     def __str__(self):
         """Print str"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.__x,
-                                                       self.__y,
-                                                       self.__width,
-                                                       self.__height)
+                                                       self.x,
+                                                       self.y,
+                                                       self.width,
+                                                       self.height)
 
     def update(self, *args, **kwargs):
         """Display func"""
@@ -107,31 +107,31 @@ class Rectangle(Base):
                     else:
                         self.id = arg
                 elif a == 1:
-                    self.__width = arg
+                    self.width = arg
                 elif a == 2:
-                    self.__height = arg
+                    self.height = arg
                 elif a == 3:
-                    self.__x = arg
+                    self.x = arg
                 elif a == 4:
-                    self.__y = arg
+                    self.y = arg
                 a += 1
 
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "id":
                     if v is None:
-                        self.__init__(self.__width,
-                                      self.__height, self.__x, self.__y)
+                        self.__init__(self.width,
+                                      self.height, self.x, self.y)
                     else:
                         self.id = v
                 elif k == "width":
-                    self.__width = v
+                    self.width = v
                 elif k == "height":
-                    self.__height = v
+                    self.height = v
                 elif k == "x":
-                    self.__x = v
+                    self.x = v
                 elif k == "y":
-                    self.__y = v
+                    self.y = v
 
     def to_dictionary(self):
         """Return dictionary"""
