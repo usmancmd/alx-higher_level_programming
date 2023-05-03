5ab90b23e8de849ef0491a79745c3d323eabb97a
Usman Adepoju <adepojuusman465@gmail.com>
2023-05-03 12:32:25 +0100

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,24 @@ class Base:
         return json.dumps(list_dictionaries)
 
     @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = str(cls.__name__) + ".json"
+        with open(filename, "w") as jsfile:
+            if list_objs is None:
+                jsfile.write("[]")
+            else:
+                for obj in list_objs:
+                    list_dicts = obj.to_dictionary()
+                jsfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
     def create(cls, **dictionary):
         """Return a class instantied from a dictionary of attributes"""
         if dictionary and dictionary != {}:
@@ -33,12 +51,12 @@ class Base:
             return new
 
     @classmethod
-    def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs to a file"""
-        with open(filename, "w") as jsfile:
-            if list_objs is None:
-                jsfile.write("[]")
-            else:
-                for obj in list_objs:
-                    list_dicts = obj.to_dictionary()
-                jsonfile.write(Base.to_json_string(list_dicts))
+    def load_from_file(cls):
+        """load_from_file(cls): that returns a list of instances"""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsfile:
+                list_dicts = Base.from_json_string(jsfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
