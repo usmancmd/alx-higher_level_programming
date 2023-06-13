263cb44ea797c2bcaf37c62a1d222df6152f9e16
Usman Adepoju <adepojuusman465@gmail.com>
2023-06-13 07:42:11 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+function add(a, b) {
+  return (Number(a)+Number(b));
+}
+
+console.log(add(num1, num2));
