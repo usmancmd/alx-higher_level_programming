3a7b25f8fd2d3d65f803e1106db754165508eef9
Usman Adepoju <adepojuusman465@gmail.com>
2023-06-14 14:16:00 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ module.exports = class Square extends require('./5-square.js') {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log('c'.repeat(this.width));
       }
     }
   }
