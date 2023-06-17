535cc851e9b259ef5a0f8ab4455f979ece0fb4f3
Usman Adepoju <adepojuusman465@gmail.com>
2023-06-17 08:12:00 +0100

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
-    if state == '':
+    if state is None:
         print('')
     else:
         print("{}: {}".format(state.id, state.name))
