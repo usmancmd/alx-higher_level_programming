06ecc5269d4290a70b6bb586beb9953fa3654bab
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-01 11:51:18 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Sends a request to that URL
-# And displays the size of the body of the response
-
+# Sends a request to a URL and displays the size of the body of the response
 curl -s $1 | wc -c
