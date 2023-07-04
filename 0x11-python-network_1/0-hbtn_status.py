f1d7460e5cd9816bfe8fcc975a93bd13f8cf1032
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-04 15:49:06 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Fetches https://alx-intranet.hbtn.io/status using urllib
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        data = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
