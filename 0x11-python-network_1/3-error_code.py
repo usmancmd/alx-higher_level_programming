a19b9dfa0d105a95a2e7cfcdfb00d08e8f2d33d8
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-05 15:38:28 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@ import urllib
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(sys.argv[2]) as res:
+        with urllib.request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
     except urllib.error.HTTPError as err:
         print('Error code:', err.code)
