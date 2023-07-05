3b419ef4f0ad949647c926501d784e4e284a3d47
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-05 15:07:10 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL
+displays the body of the response (decoded in utf-8)
+"""
+
+import sys
+import urllib 
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[2]) as res:
+            print(res.read().decode('UTF-8'))
+    except urllib.error.HTTPError as err:
+        print('Error code:', err.code)
+
