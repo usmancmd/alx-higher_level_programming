43ce9eb2f8a0f7fdfca1f9b46eb793fbde644d75
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-25 15:48:01 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,10 @@
 const request = require('request');
 const args = process.argv;
 
-request("https://swapi-api.alx-tools.com/api/films/" + `${args[2]}`, (error, response, body) => {
-    console.log(JSON.parse(body).title);
-  });
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+request(url + `${args[2]}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(JSON.parse(body).title);
+});
