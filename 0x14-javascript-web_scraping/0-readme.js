ce41cb4bfee6354a091f38d03767e1d468f55eb9
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-25 15:01:14 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,12 @@
 const fs = require('fs');
 const args = process.argv;
 
-
 fs.readFile(`${args[2]}`, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    // Print the content of the file
-    console.log(data);
+  // Print the content of the file
+  console.log(data);
 });
