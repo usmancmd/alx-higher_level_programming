5366dfead4b5b6209e2004cc34246b3a37c80efb
Usman Adepoju <adepojuusman465@gmail.com>
2023-09-18 14:36:22 +0100

    Add solution to task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2.
+
+CREATE DATABASE hbtn_0d_2;
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIELD BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhos' WITH GRANT OPTION;
